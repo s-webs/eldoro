c33139e38ee295d176814e78267fc14d53a2b6c2
Nikita V <swebs.sh@gmail.com>
2025-04-15 14:58:27 +0500

    Установил moonshine, добавил категории

---
+++ app/MoonShine/Resources/CategoryResource.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+
+use MoonShine\Laravel\Resources\ModelResource;
+use MoonShine\UI\Components\Layout\Box;
+use MoonShine\UI\Fields\ID;
+use MoonShine\Contracts\UI\FieldContract;
+use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Fields\Number;
+use MoonShine\UI\Fields\Text;
+
+/**
+ * @extends ModelResource<Category>
+ */
+class CategoryResource extends ModelResource
+{
+    protected string $model = Category::class;
+
+    protected string $title = 'Categories';
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function indexFields(): iterable
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Название', 'name_ru')->sortable(),
+            Number::make('Сортировка', 'sort_order')->sortable(),
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract|FieldContract>
+     */
+    protected function formFields(): iterable
+    {
+        return [
+            Box::make([
+                ID::make(),
+                Text::make('Название на русском', 'name_ru')->unescape(),
+                Text::make('Название на казахском', 'name_kz')->unescape(),
+                Text::make('Название на английском', 'name_en')->unescape(),
+                Number::make('Сортировка', 'sort_order')
+                    ->min(0)
+                    ->default(0)
+            ])
+        ];
+    }
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function detailFields(): iterable
+    {
+        return [
+            ID::make(),
+        ];
+    }
+
+    /**
+     * @param Category $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    protected function rules(mixed $item): array
+    {
+        return [];
+    }
+}
